28fcf57da59477bc04bac0940edd497172d29bd8
Alex Johnson <alexmj212@gmail.com>
2015-01-20 22:28:40 -0500

    removing some stuff

--- index.php
+++ index.php
@@ -1,53 +1,28 @@
 <?php
-require 'vendor/autoload.php';
-
-$app = new \Slim\Slim();
-
-$default_services = array(
-    'SSH'=>'22',
-    'SMTP'=>'25',
-    'HTTP'=>'80',
-    'MySQL'=>'3306'
-);
-
-$app->get( '/api/status/' , 'controller' );
-
-$app->run();
 
 /**
  * 
- * PHP Status Checker with REST API
  * Alex Johnson
  * 
  */
 
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get( '/api/' , 'controller' );
+
+$app->run();
+
 	function controller () {
 
-		global $default_services;
+		
 
-	    $report = server_report($default_services);
-
-	    print_service_status($report);
 	}
 
-    function server_report($services) {
-        $report = array();
-        foreach ($services as $service=>$port) {
-            $report[$service.":".$port] = check_port($port);
-        }
-        return $report;
-    }
+    function print_data($data) {
 
-    function check_port($port) {
-        $conn = @fsockopen("127.0.0.1", $port, $errno, $errstr, 2);
-        if ($conn) {
-            fclose($conn);
-            return true;
-        } else return false;
-    }
-
-    function print_service_status($services) {
-		echo json_encode($services);
+		echo json_encode($data);
     }
 
 ?>
