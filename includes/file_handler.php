00def8c3909bc5f09cc782c9b140b197d311af39
Alex Johnson <alexmj212@gmail.com>
2015-01-23 00:11:52 -0500

    documentation send function file handling some refactoring

---
+++ includes/file_handler.php
@@ -0,0 +1,85 @@
+<?php
+
+class Handler {
+
+	/*
+		{
+		    "@username": {
+		        "received": 0,
+		        "last_received_date": "2015-01-01 00:00:00",
+		        "last_received_user": "@username2",
+		        "sent": 0,
+		        "last_sent_date": "2015-01-01 00:00:00",
+		        "last_sent_user": "@username2"
+		    },
+		    "@username2": {
+		        "received": 0,
+		        "last_received_date": "2015-01-01 00:00:00",
+		        "last_received_user": "@username",
+		        "sent": 0,
+		        "last_sent_date": "2015-01-01 00:00:00",
+		        "last_sent_user": "@username"
+		    }
+		}
+	 */
+	
+	private $filename;
+
+	private $rate_limit;
+
+	public function __construct(){
+
+		$this->filename = 'tips.json';
+
+	}
+
+	public function log_tip(&$payload){
+
+		date_default_timezone_set('UTC');
+
+		$data = json_decode(file_get_contents($this->filename), true);
+
+		$this->rate_limit = strtotime('now');
+
+		//TODO: CLEAN THIS UP
+
+		if($data[$payload->user_name]['last_sent_date']){
+
+			if(strtotime($data[$payload->user_name]['last_sent_date'].' + 10 seconds') <= $this->rate_limit){
+
+				$data[$payload->user_name]['sent'] += 1;
+				$data[$payload->user_name]['last_sent_date'] = date('Y-m-d H:i:s');
+				$data[$payload->user_name]['last_sent_user'] = $payload->recipient;
+				$data[$payload->recipient]['received'] += 1;
+				$data[$payload->recipient]['last_received_date'] = date('Y-m-d H:i:s');
+				$data[$payload->recipient]['last_received_user'] = $payload->user_name;
+
+			} 
+		}
+		else {
+			$data[$payload->user_name]['sent'] += 1;
+			$data[$payload->user_name]['last_sent_date'] = date('Y-m-d H:i:s');
+			$data[$payload->user_name]['last_sent_user'] = $payload->recipient;
+			$data[$payload->recipient]['received'] += 1;
+			$data[$payload->recipient]['last_received_date'] = date('Y-m-d H:i:s');
+			$data[$payload->recipient]['last_received_user'] = $payload->user_name;
+		}
+
+
+
+		file_put_contents($this->filename, json_encode($data));
+
+	}
+
+	public function retrieve_total($user_name){
+		$data = json_decode(file_get_contents($this->filename), true);
+
+		if($data[$user_name]['received']){
+			return $data[$user_name]['received'];
+		} else return 0;
+
+	}
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,75 +1,104 @@
 <?php
 
 /**
- * 
+ * Tip Bot for Slack
  * Alex Johnson
- * 
+ * Main
+ */
+
+/**
+ * Description:
+ * * Driver for the application
+ * * Recieve data, rocess and send responses
  */
 
 require 'vendor/autoload.php';
 include 'config.php';
 include 'includes/payload_processor.php';
+include 'includes/file_handler.php';
 
+//Initialize Slim Framework
 $app = new \Slim\Slim();
 
-$sender = new ProcessPayload($app->request->get());
+$filehandler = new Handler();
 
-$app->get( '/tip/', 'tip');
+//Define 'tip' endpoint and associated controller
+$app->post( '/tip/', 'tip');
 
+//Execute Slim framework processing
 $app->run();
+
+    /**
+     * tip function
+     * Controller for the tip endpoint
+     */
 
     function tip (){
 
-        global $vars;
-        global $webhook_url;
-        global $sender;
+        global $app;  
+        global $payload;
+        global $filehandler;
 
-        $text = process_response($sender);
+        $payload = new ProcessPayload($app->request->post());
+        $payload->response_text = tip_process_response($payload);
 
-        $flag = 1;
-
-        $bot_username = "Tip Bot";
-
-        if($text){
-            $data = '{"channel": "'.$sender->channel_name.'", ';
-            $data = $data.'"username": "'.$bot_username.'", ';
-            $data = $data.'"text": "'.$text.'", ';
-            $data = $data.'"icon_emoji": ":heavy_dollar_sign:"}';
-            $ch = curl_init();
-
-            curl_setopt($ch, CURLOPT_URL,$webhook_url);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
-
-            // receive server response ...
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-            $server_output = curl_exec ($ch);
-
-            curl_close ($ch);
-
+        if($payload->payload_type != 'invalid'){
+            
+            send_response($payload);
         }
     }
 
-    function process_response($payload){
+
+
+    function tip_process_response(&$payload){
+
+        global $filehandler;
 
         switch($payload->payload_type){
             case 'user_name' :
                 if($payload->user_name == $payload->recipient){
                     echo "You can't tip yourself!";
-                    return 0;
+                    return;
                 }
-                return $payload->user_name.' has tipped '.$payload->recipient.' in the channel '.$payload->channel_name;
+                $filehandler->log_tip($payload);
+                return $payload->user_name.' has tipped '.$payload->recipient;
                 break;
             case 'total' :
-                echo "Just the tip.";
-                return 0;
+                echo "You've been tipped ".$filehandler->retrieve_total($payload->user_name)." time(s)";
+                return;
                 break;
             default :
                 echo "Invalid command!";
-                return 0;
+                return;
                 break;
         }
+    }
+
+    function send_response(&$payload){
+
+        global $webhook_url;
+
+        $bot_user_name = "Tip Bot";
+
+        $data = '{"channel": "'.$payload->channel_name.'", ';
+        $data = $data.'"username": "'.$bot_user_name.'", ';
+        $data = $data.'"text": "'.$payload->response_text.'", ';
+        $data = $data.'"icon_emoji": ":heavy_dollar_sign:"}';
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL,$webhook_url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+
+        // receive server response ...
+        //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        echo $payload->response_text;
+
+        $server_output = curl_exec ($ch);
+
+        curl_close ($ch);
+
     }
 
 ?>
