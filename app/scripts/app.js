bda1dcaecddf6c0c2d3c5df132ff9ad7590ed1c5
Alex Johnson <alexmj212@gmail.com>
2015-04-05 13:02:22 -0400

    font-awesome, angular-toarrayfilter bower updates to index.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'angular-toArrayFilter'
   ])
   .config(function ($routeProvider) {
     $routeProvider
