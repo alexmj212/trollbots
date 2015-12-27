27e603e42e971f19b5ea99319b7b8bd004e832b6
Alex Johnson <alexmj212@gmail.com>
2015-12-27 04:36:17 -0500

    Adding oauth for Slack App integration

---
+++ includes/oauth-default.php
@@ -0,0 +1,32 @@
+<?php
+
+class OAuth {
+	private $slack_url = "https://slack.com/api/oauth.access";
+
+	private $client_id;
+	private $client_secret;
+	private $code;
+
+	public function __construct($data,$botname){
+
+
+		switch($botname){
+			case "dkpbot":
+				break;
+			default:
+				break;
+		}
+		var_dump($data);
+
+	        $this->code = $data["code"];
+        	$ch = curl_init();
+		$url = $this->slack_url."?client_id=".$this->client_id."&client_secret=".$this->client_secret."&code=".$this->code;
+	        curl_setopt($ch, CURLOPT_URL,$url);
+        	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	        $response = curl_exec ($ch);
+        	curl_close ($ch);
+		header('Location: http://alexmj212.github.io/slackphpbot/?success='.$botname);
+	}
+
+}
+
--- index.php
+++ index.php
@@ -14,6 +14,7 @@ require 'vendor/autoload.php';
 include 'includes/processPayload.php';
 include 'includes/responseHandler.php';
 include 'includes/dataSource.php';
+include 'includes/oauth.php';
 foreach (glob('scripts/*.php') as $filename)
 {
     include_once $filename;
@@ -27,29 +28,30 @@ $app->get('/', 'main');
 
 //Define 'tip' endpoint
 $app->post('/tipbot/', function() use ($app) {$tipbot = new TipBot($app->request->post());});
+//$app->get('/tipbot-auth/', function() use ($app) {$tipbotauth = new OAuth($app->request->get(),"tipbot");});
 
 //Define 'triggered' endpoint
 $app->post('/triggerbot/', function() use ($app) {$triggerbot = new TriggerBot($app->request->post());});
+//$app->get('/triggerbot-auth/', function() use ($app) {$triggerbotauth = new OAuth($app->request->get(),"triggerbot");});
 
 //Define 'channelpolice' endpoint
 $app->post('/channelpolicebot/', function() use ($app) {$channelpolicebot = new ChannelPoliceBot($app->request->post());});
+//$app->get('/channelpolicebot-auth/', function() use ($app) {$channelpolicebotauth = new OAuth($app->request->get(),"channelpolicebot");});
 
 //Define 'punbot' endpoint
 $app->post('/punbot/', function() use ($app) {$punbot = new PunBot($app->request->post());});
+//$app->get('/punbot-auth/', function() use ($app) {$punbotauth = new OAuth($app->request->get(),"punbot");});
 
 //Define 'dkpbot' endpoint
 $app->post('/dkpbot/', function() use ($app) {$dkpbot = new DKPBot($app->request->post());});
-
+$app->get('/dkpbot-auth/', function() use ($app) {$dkpbotauth = new OAuth($app->request->get(),"dkpbot");});
 
 //Run the app
 $app->run();
 
 //Redirect main page (optional)
 	function main (){
-		header('Location: https://github.com/alexmj212/slackphpbot');
+		header('Location: http://alexmj212.github.io/slackphpbot/');
 		die();
 	}
-
-
-
 ?>
