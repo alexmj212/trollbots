f08c6ddda915d801bd48e1fcd32ecf86e791f00d
Alex Johnson <alexmj212@gmail.com>
2016-02-23 09:26:11 -0500

    Make sure it's a very clear warning

--- scripts/TriggerBot.php
+++ scripts/TriggerBot.php
@@ -41,7 +41,7 @@ class TriggerBot extends Bot
         if (Payload::isUserName($payload->getText()) === true) {
             $response .= ' *'.$payload->getText().'*!';
         } else if ($payload->getText() === 'warning') {
-            $response = 'TRIGGER WARNING';
+            $response = ':heavy_exclamation_mark::heavy_exclamation_mark::heavy_exclamation_mark: :warning: :rotating_light: :warning: TRIGGER WARNING :warning: :rotating_light: :warning::heavy_exclamation_mark::heavy_exclamation_mark::heavy_exclamation_mark:';
         } else {
             $response = Post::INVALID_COMMAND;
         }
